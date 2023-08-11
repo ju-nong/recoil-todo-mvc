74833f0a4840d8b6e7fa75ddda5dd622062e5699
이준용 <iiiuooyoo@gmail.com>
2023-08-11 13:35:27 +0900

    init

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+
+import "./assets/css/style.scss";
+
+import { RecoilRoot } from "recoil";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <RecoilRoot>
+        <App />
+    </RecoilRoot>,
+);
